6779b33cdd8a0d84533cd300cc9d8894f3d533d2
shinichihatano <monntatarou2008@gmail.com>
2022-06-05 08:53:08 +0900

    画面遷移後にFloat型で計算されるように修正

--- app/src/main/java/com/example/calcapp/SecondActivity.kt
+++ app/src/main/java/com/example/calcapp/SecondActivity.kt
@@ -10,10 +10,8 @@ class SecondActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
 
-        var edittext4 = intent.getIntExtra("VALUE1",0)
-        var edittext5 = intent.getIntExtra("VALUE2",0)
-        var edittext6 = edittext4 + edittext5
-
-        textview.text = "$edittext6"
+        var edittext4 = intent.getFloatExtra("VALUE1",0F)
+        var edittext5 = intent.getFloatExtra("VALUE2",0F)
+        textview.text = "${edittext4 + edittext5}"
     }
 }
